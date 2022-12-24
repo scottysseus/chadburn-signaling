28d339f598b95ac0eace6263bb2dd66cdbd2c688
Scott Weidenkopf <sweidenk@google.com>
2022-12-24 18:03:48 +0000

    add eslint and pre-commit hook

--- dist/index.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-const app = express();
-const port = 8001;
-app.get("/", (req, res) => {
-    res.status(200).send("hello world");
-});
-app.listen(port, () => {
-    console.log(`signaling server started on port ${port}`);
-});
